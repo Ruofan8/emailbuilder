e211af39e248427ba4851c26eb6fe3915ad4877a
Ruofan Wang <r.wang@brunel.net>
2017-07-12 16:08:08 +0200

    testing mandrilapi

--- src/app/builder/builder.component.ts
+++ src/app/builder/builder.component.ts
@@ -3,7 +3,7 @@ import { BuilderService} from './builder.service'
 import { TextComponent } from './text/text.component';
 import { ButtonComponent } from './button/button.component';
 import * as FileSaver from 'file-saver';
-
+import mandrill from 'mandrill-api/mandrill';
 @Component({
   selector: 'app-builder',
   templateUrl: './builder.component.html',
@@ -12,31 +12,41 @@ import * as FileSaver from 'file-saver';
 
 export class BuilderComponent implements OnInit {
   builders: Array<Object>;
+  buildersString: string;
 
-  constructor(private service: BuilderService) { }
+  constructor(private service: BuilderService) {  }
   ngOnInit() {
     this.builders = this.service.getTemplate();
+    this.buildersString = JSON.stringify(this.builders);
+    const mandrill_client = new mandrill.Mandrill('PS9odNH5RBfJUncs07L3gA');
+    const template = {
+      name: 'JobAlert Activation - BE',
+      text: this.buildersString
+    };
+    mandrill_client.templates.update(template, function(result) {
+        console.log(result);
+    });
   }
   exportHTML()  {
-    var appBuilder:any  = document.querySelectorAll('app-builder')[0].innerHTML
-    var blob = new Blob([appBuilder], {type: "text/plain;charset=utf-8"});
-    //TODO: missing css
-    FileSaver.saveAs(blob, "rendered.html");
+    const appBuilder:any  = document.querySelectorAll('app-builder')[0].innerHTML
+    const blob = new Blob([appBuilder], {type: 'text/plain;charset=utf-8'});
+    // TODO: missing css
+    FileSaver.saveAs(blob, 'rendered.html');
   }
   exportJSON() {
-    var blob = new Blob([JSON.stringify(this.builders)], {type: "text/plain;charset=utf-8"});
-    FileSaver.saveAs(blob, "template.txt");
+    const blob = new Blob([JSON.stringify(this.builders)], {type: 'text/plain;charset=utf-8'});
+    FileSaver.saveAs(blob, 'template.txt');
   }
   importJSON($event) : void {
-    var file:File = $event.target.files[0];
-    var myReader:FileReader = new FileReader();
+    const file:File = $event.target.files[0];
+    const myReader:FileReader = new FileReader();
 
     myReader.onloadend = (e) => {
       if(this.isJson(myReader.result)){
         this.service.setTemplate(JSON.parse(myReader.result));
         this.builders = this.service.getTemplate();
       } else {
-        alert("Invalid builder JSON")
+        alert('Invalid builder JSON');
       }
     }
 
